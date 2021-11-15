2dfd89e7ddab96236008e3215c1fa1b7d50ff861
mortadha-dev <mortadha.bouallagui@esprit.tn>
2021-11-15 18:08:35 +0100

    adding grafana && prometheus

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
 
     public static void main(String[] args) {
